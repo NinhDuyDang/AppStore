87e2fb33b927d475006368928de4d7719274f1ed
Ninh Dang <92087915+NinhDuyDang@users.noreply.github.com>
2022-09-06 16:57:22 +0700

    Update ApplicationRepo.java

--- quanliapp-master/src/main/java/com/example/tmp/repository/ApplicationRepo.java
+++ quanliapp-master/src/main/java/com/example/tmp/repository/ApplicationRepo.java
@@ -9,4 +9,10 @@ import java.util.Optional;
 @Repository
 public interface ApplicationRepo extends JpaRepository<Application,Integer> {
     Optional<Application> findApplicationById( Integer id);
+    
+    @Query("SELECT p FROM Application p WHERE " +
+            "p.name LIKE CONCAT('%',:query, '%')" +
+            "Or p.description LIKE CONCAT('%', :query, '%')")
+    List<Application> searchApplicationBy(String query);
+    
 }
